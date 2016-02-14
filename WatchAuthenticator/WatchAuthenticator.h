ba77f0d6372a9c3b03d172eed7686484ea1f26bb
Pedro Mancheno <pedromancheno@gmail.com>
2016-02-14 15:34:41 +0100

    Initial Commit

---
+++ WatchAuthenticator/WatchAuthenticator.h
@@ -0,0 +1,19 @@
+//
+//  WatchAuthenticator.h
+//  WatchAuthenticator
+//
+//  Created by Pedro Mancheno on 14/02/16.
+//  Copyright © 2016 pedromancheno. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for WatchAuthenticator.
+FOUNDATION_EXPORT double WatchAuthenticatorVersionNumber;
+
+//! Project version string for WatchAuthenticator.
+FOUNDATION_EXPORT const unsigned char WatchAuthenticatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchAuthenticator/PublicHeader.h>
+
+
